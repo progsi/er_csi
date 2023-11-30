5a4714340e44b87048b67fe2491605ea3d00d5be
progsi <simon@hachmeier.de>
2023-11-30 12:49:13 +0100

    with prepare data

--- train.py
+++ train.py
@@ -6,7 +6,6 @@ from torch.optim import Adam
 from sentence_transformers import models, SentenceTransformer
 
 
-
 #Define the model. Either from scratch of by loading a pre-trained model
 model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
 
@@ -20,7 +19,7 @@ dataset_train = OnlineCoverSongDataset(
     config.yt_metadata_file)
     
     
-#Define your train dataset, the dataloader and the train loss
+# Define your train dataset, the dataloader and the train loss
 train_dataloader = DataLoader(dataset_train, shuffle=True, batch_size=1)
 
 # Define the model
@@ -40,13 +39,13 @@ for epoch in range(2):  # adjust as needed
         
         # FIXME: this is ugly and needs to be fixed
         ref_sentence = ref_sentence["yt_title"][0]
-        positive_example["yt_title"][0]
+        positive_example = positive_example["yt_title"][0]
         negative_examples = [s["yt_title"][0] for s in negative_examples]
         
         # Forward pass
-        ref_embedding = model(ref_sentence)
-        positive_embedding = model(positive_example)
-        negative_embeddings = [model(neg_example) for neg_example in negative_examples]
+        ref_embedding = model.encode(ref_sentence)
+        positive_embedding = model.encode(positive_example)
+        negative_embeddings = [model.encode(neg_example) for neg_example in negative_examples]
 
         # Compute loss
         loss = train_loss(ref_embedding, positive_embedding, negative_embeddings)
